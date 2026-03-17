1a9ae99628120b7370e42843bea37015d3da2480
AMINE <amineelhailaa@gmail.com>
2026-03-17 17:47:30 +0000

    adding API route bootstrap and versioned endpoint groups

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,20 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\CourseController;
+use App\Http\Controllers\Api\V1\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::apiResource('courses', CourseController::class);
+
+    Route::middleware('auth:api')->post('/courses/{course}/join', [StudentController::class, 'joinCourse']);
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-
+});
